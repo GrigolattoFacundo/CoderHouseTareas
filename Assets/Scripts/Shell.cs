5bafc25aab2dbd6b4dc4eaa3c51f29ae95e118a7
GrigolattoFacundo <grigolatto122@gmail.com>
2022-03-14 16:28:27 -0300

    added damage float
    revisé la consigna antes de entregar y me flató la variable damage. la hice un float debido a la matemática que incluí.

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -6,6 +6,14 @@ public class Shell : MonoBehaviour
 {
     public float speed;
     public Vector3 shellDirection;
+    public float shellDamage;
+    public float cannonCaliber = 0.88f;
+    private void Start()
+    {
+        shellDamage = (speed + cannonCaliber);
+    }
+        
+   
     void Update()
     {
         transform.position += shellDirection * speed * Time.deltaTime; 
